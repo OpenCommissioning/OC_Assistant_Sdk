97df637f4bd9e876c07ccab61fbe2c07992e4cd5
Michael Kasseckert <michael.kasseckert@spiratec.com>
2025-11-27 15:26:59 +0100

    refactor: StringExtension

--- OC.Assistant.Sdk/StringExtension.cs
+++ OC.Assistant.Sdk/StringExtension.cs
@@ -5,8 +5,9 @@ namespace OC.Assistant.Sdk;
 /// <summary>
 /// <see cref="string"/> extension methods.
 /// </summary>
-public static class StringExtension
+public static partial class StringExtension
 {
+    
     /// <param name="value">The input string to be converted.</param>
     extension(string value)
     {
@@ -16,9 +17,8 @@ public static class StringExtension
         public int[] ToNumberList()
         {
             var res = new List<int>();
-            const string pattern = @"(?<val>((?<low>\d+)\s*?-\s*?(?<high>\d+))|(\d+))";
 
-            foreach (Match m in Regex.Matches(value, pattern))
+            foreach (Match m in NumberListRegex().Matches(value))
             {
                 if (!m.Groups["val"].Success) continue;
                 
@@ -40,29 +40,6 @@ public static class StringExtension
         }
 
         /// <summary>
-        /// Removes the description in brackets, e.g. KF2.1 (EL1008) => KF2.1
-        /// </summary>
-        public string TcRemoveBrackets()
-        {
-            const string pattern = @"(\S+)\(\S+\)";
-            var regex = new Regex(pattern, RegexOptions.IgnoreCase);
-            var match = regex.Match(value.Replace(" ", ""));
-            return match.Success ? match.Groups[1].ToString() : value;
-        }
-
-        /// <summary>
-        /// Replaces any character except a-z A-Z and 0-9 with a underscore.<br/>
-        /// Underscores at start and end will be removed.
-        /// </summary>
-        public string TcPlcCompatibleString()
-        {
-            var str =  Regex.Replace(value, "[^a-zA-Z0-9]+", "_");
-            str = str.StartsWith('_') ? str.Remove(0, 1) : str;
-            str = str.EndsWith('_') ? str.Remove(str.Length - 1) : str;
-            return str;
-        }
-
-        /// <summary>
         /// Returns the string with the first letter capitalized.
         /// </summary>
         public string FirstCharToUpper()
@@ -71,18 +48,17 @@ public static class StringExtension
             return value.Length == 1 ? $"{char.ToUpper(value[0])}" : $"{char.ToUpper(value[0])}{value[1..]}";
         }
 
-        /// Converts the input string to a format compatible with PLC naming conventions.
-        /// <return>Returns the PLC-compatible string. If the input string is already PLC-compatible, it is returned unchanged; otherwise, it is wrapped with backticks (`).</return>
-        public string MakePlcCompatible()
-            => value.IsPlcCompatible() ? value : $"`{value}`";
-
         /// <summary>
-        /// Checks if the input string is compatible with PLC naming conventions.
+        /// Checks if the input string only contains the characters <c>a-z</c> <c>A-Z</c> <c>0-9</c> and <c>_</c>
         /// </summary>
-        /// <returns>Returns <c>true</c> if the input string is PLC compatible; otherwise, <c>false</c>.</returns>
-        public bool IsPlcCompatible()
-            => !string.IsNullOrEmpty(value) && !InvalidCharacters.IsMatch(value);
+        /// <returns>True if the input string only contains basic characters, otherwise False.</returns>
+        public bool IsBasicCharacters()
+            => !string.IsNullOrEmpty(value) && !BasicCharactersRegex().IsMatch(value);
     }
-
-    private static Regex InvalidCharacters { get; } = new ("[^a-zA-Z0-9_]+");
+    
+    [GeneratedRegex(@"(?<val>((?<low>\d+)\s*?-\s*?(?<high>\d+))|(\d+))")]
+    private static partial Regex NumberListRegex();
+    
+    [GeneratedRegex("[^a-zA-Z0-9_]+")]
+    private static partial Regex BasicCharactersRegex();
 }
